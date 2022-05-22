3feafd935a4e4a79f6dfba59a6deab17677ba0a8
Toysin <toysin1203@hotmail.com>
2022-05-22 21:45:11 +0900

    계산기 완료

---
+++ src/subject6/ex1/Calc.java
@@ -0,0 +1,12 @@
+package subject6.ex1;
+
+public interface Calc {
+
+    double PI = 3.14;
+    int ERROR = -999999999;
+
+    int add(int num1, int num2);
+    int substract(int num1, int num2);
+    int times(int num1, int num2);
+    int divide(int num1, int num2);
+}
---
+++ src/subject6/ex1/Calculator.java
@@ -0,0 +1,24 @@
+package subject6.ex1;
+
+public class Calculator implements Calc {
+
+    @Override
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int substract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public int times(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
