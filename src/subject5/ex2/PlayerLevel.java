1040aaa98dd4319697e1e49a1ef9403c18f30c92
Toysin <toysin1203@hotmail.com>
2022-05-22 21:36:21 +0900

    void go() 메서드 오버라이딩 불가능으로 바꿈 -> final void go()

--- src/subject5/ex2/PlayerLevel.java
+++ src/subject5/ex2/PlayerLevel.java
@@ -7,7 +7,7 @@ public abstract class PlayerLevel {
     public abstract void turn();
     public abstract void showLevelMessage();
 
-    public void go(int number) {
+    public final void go(int number) {
         showLevelMessage();
         run();
         for (int i = 0; i < number; i++) {
